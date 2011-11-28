516991fe6567ac4cad17905d4cefa7eb334bacdc
Ishank Gupta <ishank.gupta@vinsol.com>
2011-11-28 17:50:31 +0530

    Initial Repo Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IshankAssetMgmt::Application.config.secret_token = '7b106ec8e2f85f40641536378ca2e085c7fb5fb2493fd2069de18ddbba86c068e2fd634f8c75f90e4cf620bc218c2fd95e03ca8bfbd383a4a418aecd66389425'
