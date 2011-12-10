49c9a3c1dbaf1f5927259e486e3e57d91b049d46
Ishank Gupta <ishank.gupta@vinsol.com>
2011-12-10 17:29:36 +0530

    Added Currnecy And Vendor to Asset form

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,3 +3,5 @@ CATEGORY=['Laptop', 'laptop'], ['Network Device', 'network_device'], ['Mobile Ph
 STATUS=['Operational', 'operational'], ['Recieved', 'recieved'], ['Spare', 'spare'], ['Repair', 'repair'], ['Assigned', 'Assigned']
 
 CATEGORY_CLASS=['Laptop', 'Laptop'], ['Network Device', 'NetworkDevice'], ['Mobile Phone', 'MobilePhone']
+
+CURRENCY=['INR', '&#8377;'], ['USD', '&#36;'], ['GBP', '&#163;']
