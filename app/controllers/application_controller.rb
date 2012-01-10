6fce6647026b296191862a26a698df2e5a762964
Ishank Gupta <ishank.gupta@vinsol.com>
2012-01-10 13:30:56 +0530

    Implemented changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 class ApplicationController < ActionController::Base
+
 	before_filter :authenticate_admin!
   protect_from_forgery
-  def string_to_date str
-		!str.blank? ? DateTime.strptime(str, "%m/%d/%Y") : nil
-  end
-  
-  def date_to_string date_obj
-		!date_obj.blank? ? date_obj.strftime("%m/%d/%Y") : nil
-  end
-  	
-
-
-  	
+    	
 end
