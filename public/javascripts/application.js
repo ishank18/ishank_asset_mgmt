491ecf38fc14f9a21a7e3532f578985736391ec1
Ishank Gupta <ishank.gupta@vinsol.com>
2012-01-19 14:20:08 +0530

    added ajax progress status

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function showProgress() {
+	$("#progressContainer").html("<div class='progress'></div><div class='loadingText'>Loading...</div>")
+}
+
+function endProgress() {
+	$("#progressContainer").html("")
+}
+
