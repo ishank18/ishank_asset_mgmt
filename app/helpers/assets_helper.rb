eb5fb40499bd672feaec6ce1e97e52ca28c0c854
Ishank Gupta <ishank.gupta@vinsol.com>
2012-01-31 14:02:32 +0530

    added truncate in remark, removed received for new asset, resolved assign asset form issue

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -10,6 +10,19 @@ module AssetsHelper
 		end
 	end
 	
+	def show_status_field f
+		if @asset.status == "Assigned"
+			"Assigned"
+		else 
+			if @asset.new_record?
+				status_wo_recieved = STATUS.dup
+				status_wo_recieved.delete("Recieved")
+				f.select(:status, options_for_select(status_wo_recieved.to_a[0...status_wo_recieved.length-1], @asset.status), :include_blank => "- Select -")
+			else
+				f.select(:status, options_for_select(STATUS.to_a[0...STATUS.length-1], @asset.status), :include_blank => "- Select -")
+			end
+		end
+	end
 	
 	def show_category asset
 		if !asset.new_record?
@@ -18,8 +31,16 @@ module AssetsHelper
 			select_tag "asset[resource_type]", options_for_select(CATEGORY, @asset.resource_type), :include_blank => "- Select -", :onchange => "changeForm()"	
 		end
 	end
-	
-	
 
-	
+	def fetch_assets
+		options = []
+		Asset.where(%{resource_type = ? and status not in ('#{STATUS["Assigned"]}', '#{STATUS["Repair"]}')}, params[:category]).each do |asset|
+				currOpt = []
+				currOpt << "#{asset.id} - #{asset.name}"
+				currOpt << asset.id
+				options << currOpt
+		end
+		options
+	end	
+		
 end
