4ce837b74f36d530937ba76b7c1e875f7ab0c608
Ishank Gupta <ishank.gupta@vinsol.com>
2012-01-31 12:19:01 +0530

    aletered migration for is_active in AEM

--- db/migrate/20120130114333_drop_status_add_active_on_aem.rb
+++ db/migrate/20120130114333_drop_status_add_active_on_aem.rb
@@ -1,11 +1,31 @@
 class DropStatusAddActiveOnAem < ActiveRecord::Migration
   def self.up
   	add_column :asset_employee_mappings, :is_active, :boolean, :default => true
+  	AssetEmployeeMapping.reset_column_information
+  	AssetEmployeeMapping.all.each do |aem|
+			if(aem.status == "Assigned")
+				@sql = "update asset_employee_mappings set is_active = '1' where id = '#{aem.id}'"
+			else
+				@sql = "update asset_employee_mappings set is_active = '0' where id = '#{aem.id}'"
+			end
+			execute(@sql) 
+  	end  	
   	remove_column :asset_employee_mappings, :status
   end
 
   def self.down
+		add_column :asset_employee_mappings, :status, :string
+		
+  	AssetEmployeeMapping.reset_column_information
+  	AssetEmployeeMapping.all.each do |aem|
+			if(aem.is_active)
+				@sql = "update asset_employee_mappings set status = 'Assigned' where id = '#{aem.id}'"
+			else
+				@sql = "update asset_employee_mappings set status = 'returned' where id = '#{aem.id}'"
+			end
+			execute(@sql) 
+  	end
   	remove_column :asset_employee_mappings, :is_active
-  	add_column :asset_employee_mappings, :status, :string
+  	
   end
 end
