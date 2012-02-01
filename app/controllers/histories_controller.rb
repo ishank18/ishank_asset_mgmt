1a5e2ba9152ac4dd3addc69c1acc428f9b475bfc
Ishank Gupta <ishank.gupta@vinsol.com>
2012-02-01 11:55:42 +0530

    Temp changes on histories controlller

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,9 +1,9 @@
 class HistoriesController < ApplicationController
 	def index
 		if(params[:type] == "employee")
-			@histories = AssetEmployeeMapping.where(:employee_id => params[:id]).includes(:employee)
+			@histories = AssetEmployeeMapping.where(:employee_id => params[:id]).includes(:asset).includes(:employee)
 		else
-			@histories = AssetEmployeeMapping.where(:asset_id => params[:id]).includes(:asset)
+			@histories = AssetEmployeeMapping.where(:asset_id => params[:id]).includes(:asset).includes(:employee)
 		end	
 	end
 end
