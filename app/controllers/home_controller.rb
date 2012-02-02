a5c1fe83f8660356cba6860375d9a813a759c0be
Ishank Gupta <ishank.gupta@vinsol.com>
2012-02-02 22:47:00 +0530

    Pagination in search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,50 +8,23 @@ class HomeController < ApplicationController
   # pagination
 	## Will search assets and employees and will filter it according to status and category
 	def search
+		@result = []
 		asset, employee, status, category = params[:query_for_asset],  params[:query_for_emp], params[:status], params[:category]	
 		if(asset.present? or category.present? or status.present?)		
 			@result = Asset.where("assets.name like ?", "%#{asset}%")
 			if(category.present?)
-			  
-        # (category.present? ? Asset : @result).where("assets.resource_type = ?", category) # Use send if required
-			  
-				if(@result.blank?)
-					@result = Asset.where("assets.resource_type = ?", category)
-				else
-					@result = @result.where("assets.resource_type = ?", category)
-				end
+				@result = (@result.blank? ? Asset : @result).where("assets.type = ?", category) # Use send if required
 			end
 			if(status.present?)
-				if(@result.blank?)
-					@result = Asset.where("assets.status = ?", status)
-				else
-					@result = @result.where("assets.status = ?", status)
-				end
+				@result = (@result.blank? ? Asset : @result).where("assets.status = ?", status)
 			end
 			if(employee.present?)
 				@result = @result.joins(:assignments).joins(:employees).where("employees.name like ? and asset_employee_mappings.is_active = '1'", "%#{employee}%")
 			end
 		elsif(employee.present?)
 			@result = Employee.where("employees.name like ?", "%#{employee}%")
-
-		else
-			@result = []	
 		end
-		
-		
-		
-#		if((!asset.blank? or !status.blank? or !category.blank?) && !employee.blank?)
-#			@record = Assignment.joins(:asset).joins(:employee).where("assets.name like ? and employees.name like ? and asset_employee_mappings.is_active = '1'", "%#{asset}%", "%#{employee}%")
-#			unless status.blank?
-#				@record = @record.where("assets.status = ?", status)
-#			end	
-#			unless category.blank?
-#				@record = @record.where("assets.resource_type = ?", category)
-#			end
-#			@result = @record.collect { |aem| aem.asset }
-#			
-#		end
-		#@result = Assignment.search params[:query_for_asset],  params[:query_for_emp], params[:status], params[:category]		
+		@result = (@result.paginate :page => params[:page], :per_page => 2) unless @result.blank?
 	end
 	
 	## Will show assets in  the tags in the home page - Using AJAX
